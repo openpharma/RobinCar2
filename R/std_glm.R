2061a90c307090ad0d6e331d6a540269e7e0c996
Liming Li <liming.li@roche.com>
2023-09-03 14:54:15 +0000

    create std_glm function

---
+++ R/std_glm.R
@@ -0,0 +1,36 @@
+#' Standardized Generalized Linear Models
+#'
+#' @description `r lifecycle::badge("experimental")`
+#'
+#' @param formula (`formula`)\cr the model formula. Must include `trt` to indicate the treatment variable.
+#' @param data (`data.frame`)\cr the dataset to be used.
+#' @param trt (`string`)\cr the treatment variable.
+#' @param ... additional arguments for `glm()`.
+#'
+#' @note Response must be binary.
+#' @export
+#'
+#' @examples
+#' std_glm(Sepal.Length ~ Species, data = iris, trt = "Species")
+std_glm <- function(formula, data, trt, ...) {
+  assert_formula(formula)
+  assert_string(trt)
+  assert_subset(trt, all.vars(formula[[length(formula)]]))
+  trt_vars <- h_obtain_treat(formula, trt)
+  if (!trt %in% trt_vars) {
+    stop(sprintf("Treatment variable %s should exisit in the right hand side of formula!", trt))
+  }
+
+  fit <- glm(formula = formula, data = data, family = binomial(link = "logit"), ...)
+  res <- list(fit = fit)
+  res$trt <- trt
+  res$data <- data
+  trt_levels <- unique(data[[trt]])
+  trt_coef <- paste0(trt, trt_levels)
+  trt_level_match <- match(trt_coef, names(coef(fit)))
+  reference_arm <- trt_levels[is.na(trt_level_match)]
+  res$trt_levels <- levels(data[[trt]])[-1L]
+  res$ref_level <- reference_arm
+  class(res) <- "std_glm"
+  res
+}
--- R/treatment_effect.R
+++ R/treatment_effect.R
@@ -20,12 +20,83 @@ treatment_effect.std_lm <- function(x, trt, ref, vcov_method = "HC3", ...) {
     trt = trt,
     ref = ref,
     stat = "diff",
-    coef = sum(sel_vec * coefs),
-    robust_var = sel_vec %*% cov %*% sel_vec
+    est = sum(sel_vec * coefs),
+    vcov_method = vcov_method,
+    var = (sel_vec %*% cov %*% sel_vec)[1, 1]
+  )
+  return(ret)
+}
+#' @export
+treatment_effect.std_glm <- function(x, trt, ref, vcov_method = "HC3", statistic = "rr", ...) {
+  if (missing(ref)) {
+    ref <- x$ref_level
+  }
+  if (missing(trt)) {
+    trt <- x$trt_levels[1]
+  }
+  if (test_string(statistic)) {
+    assert_subset(statistic, c("rr", "or", "logor", "logrr", "diff"))
+    stat <- match.fun(statistic)
+    grad <- match.fun(paste0("grad_", statistic))
+  } else {
+    stop("Unknown statistic")
+  }
+  assert_subset(c(trt, ref), c(x$trt_levels, x$ref_level))
+  df <- x$data
+  df[[x$trt]] <- factor(trt, levels = sort(unique(x$data[[trt]])))
+  pred1 <- predict(x$fit, data = df, type = "response")
+  mat1 <- model.matrix(x$fit$formula, data = df)
+  df[[x$trt]] <- factor(ref, levels = sort(unique(x$data[[trt]])))
+  pred2 <- predict(x$fit, data = df, type = "response")
+  mat2 <- model.matrix(x$fit$formula, data = df)
+  m1 <- mean(pred1)
+  m2 <- mean(pred2)
+  est <- stat(m1, m2)
+  gr <- grad(m1, m2)
+  cov <- sandwich::vcovHC(x$fit, type = vcov_method)
+  n <- nrow(df)
+  mm <- rbind(c(pred1 * (1 - pred1) / n, rep(0, n)), c(rep(0, n), pred2 * (1 - pred2) / n))
+  d <- rbind(mat1, mat2)
+  cov_m <- mm %*% d %*% cov %*% t(d) %*% t(mm)
+  var <- t(gr) %*% cov_m %*% gr
+  ret <- list(
+    trt = trt,
+    ref = ref,
+    stat = statistic,
+    est = est,
+    vcov_method = vcov_method,
+    var = var[1, 1]
   )
   return(ret)
 }
 
-treatment_effect.std_glm <- function(x, level1, level2, statistic = c("rr", "or"), ...) {
-  stop("Not implemented")
+rr <- function(x, y) {
+  x / y
+}
+grad_rr <- function(x, y) {
+  c(1 / y, -x / y^2)
+}
+or <- function(x, y) {
+  x / (1 - x) / y * (1 - y)
+}
+grad_or <- function(x, y) {
+  c((1 - y) / ((1 - x)^2 * y), -x / ((1 - x) * y^2))
+}
+logrr <- function(x, y) {
+  log(rr(x, y))
+}
+grad_logrr <- function(x, y) {
+  c(1 / x, -1 / y)
+}
+logor <- function(x, y) {
+  log(or(x, y))
+}
+grad_logor <- function(x, y) {
+  c(1 / (x * (1 - x)), -1 / (y * (1 - y)))
+}
+diff <- function(x, y) {
+  x - y
+}
+grad_diff <- function(x, y) {
+  c(1, -1)
 }
