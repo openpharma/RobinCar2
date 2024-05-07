07303607f6ace2b30ecfc4a0ff3591581230c30e
Liming Li （李黎明） <liming.li@roche.com>
2024-05-07 13:02:28 +0000

    simplify

--- R/bias.R
+++ R/bias.R
@@ -28,7 +28,7 @@ bias <- function(residual, treatment, strata, data) {
   d <- matrix(NA_real_, nrow = nrow(data), ncol = length(trt_lvls))
   id_strat <- split(seq_len(nrow(data)), strat_var)
   for (i in id_strat) {
-    df <- vapply(split(residual[i], trt_var[i]), function(xx) mean(xx), FUN.VALUE = 0)
+    df <- vapply(split(residual[i], trt_var[i]), mean, FUN.VALUE = 0)
     d[i, ] <- matrix(df, nrow = length(i), ncol = length(df), byrow = TRUE)
   }
   d
