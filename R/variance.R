c6d8519a2617dd79a67846badc5410136ff58d3e
Liming Li （李黎明） <liming.li@roche.com>
2024-05-10 02:44:30 +0000

    correct issue

--- R/variance.R
+++ R/variance.R
@@ -3,8 +3,9 @@ vcovHC.prediction_cf <- function(x, type = "HC3",...) {
   fit <- attr(x, "fit")
   vc <- vcovHC(fit, type = type)
   mm <- attr(x, "model_matrix")
-  md <- family(fit)$mu.eta(attr(x, "predictions_linear")) / nrow(mm)
-  z <- block_sum(as.vector(md) * mm, length(md) / length(names(x)))
+  n <- length(md) / length(names(x))
+  md <- family(fit)$mu.eta(attr(x, "predictions_linear")) / n
+  z <- block_sum(as.vector(md) * mm, n)
   ret <- z %*% vc %*% t(z)
   dimnames(ret) <- list(names(x), names(x))
   ret
