160f8cc0c957284e250876c507e9dcca8d75e222
Liming Li （李黎明） <liming.li@roche.com>
2024-06-20 07:17:31 +0000

    update anhecova variance and remove report functions

--- tests/testthat/test-variance.R
+++ tests/testthat/test-variance.R
@@ -1,34 +1,16 @@
-test_that("robincar convariance estimation works for guassian fitted by lm", {
+test_that("vcovHC works", {
+  pc <- predict_counterfactual(fit_binom, treatment ~ s1)
   expect_snapshot(
-    predict_counterfactual(fit_lm, "treatment", dummy_data) |> vcovRobinCar.prediction_cf()
-  )
-
-  expect_snapshot(
-    predict_counterfactual(fit_lm, "treatment", dummy_data) |> vcovRobinCar.prediction_cf(eff_measure = "diff")
+    vcovHC(pc)
   )
 })
 
-test_that("robincar convariance estimation works for guassian fitted by glm", {
+test_that("vcovANHECOVA works", {
+  pc <- predict_counterfactual(fit_binom, treatment ~ s1)
   expect_snapshot(
-    predict_counterfactual(fit_glm, "treatment", dummy_data) |> vcovRobinCar.prediction_cf()
-  )
-
-  expect_snapshot(
-    predict_counterfactual(fit_glm, "treatment", dummy_data) |> vcovRobinCar.prediction_cf(eff_measure = "diff")
-  )
-})
-
-test_that("robincar convariance estimation works for binomial fitted by glm", {
-  expect_snapshot(
-    predict_counterfactual(fit_binom, "treatment", dummy_data) |> vcovRobinCar.prediction_cf()
+    vcovANHECOVA(pc)
   )
   expect_snapshot(
-    predict_counterfactual(fit_binom, "treatment", dummy_data) |> vcovRobinCar.prediction_cf(eff_measure = "diff")
-  )
-  expect_snapshot(
-    predict_counterfactual(fit_binom, "treatment", dummy_data) |> vcovRobinCar.prediction_cf(eff_measure = "risk ratio")
-  )
-  expect_snapshot(
-    predict_counterfactual(fit_binom, "treatment", dummy_data) |> vcovRobinCar.prediction_cf(eff_measure = "odds ratio")
+    vcovANHECOVA(pc, randomization = "permute_block")
   )
 })
