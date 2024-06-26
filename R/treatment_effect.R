46192cf53a9caeb51ac53985a603ca096be3b0de
Liming Li （李黎明） <liming.li@roche.com>
2024-06-26 10:01:14 +0000

    add treatment eff

---
+++ R/treatment_effect.R
@@ -0,0 +1,107 @@
+#' Treatment Effect
+#' @description Obtain treatment effect and variance from counter-factual prediction
+#'
+#' @param object Object from which to obtain treatment effect.
+#' @param pair (`integer` or `character`) Names or index of the treatment levels.
+#' @param variance (`function`) Variance function.
+#' @param eff_measure (`function`) Treatment effect measurement function.
+#' @param eff_jacobian (`function`) Treatment effect jacobian function.
+#' @param ... Additional arguments for variance.
+#'
+#' @export
+treatment_effect <- function(object, pair, variance, eff_measure, eff_jacobian, ...) {
+  UseMethod("treatment_effect")
+}
+
+#' @export
+treatment_effect.prediction_cf <- function(object, pair = names(object), variance = vcovANHECOVA, eff_measure, eff_jacobian, ...) {
+  assert(
+    test_function(variance),
+    test_null(variance)
+  )
+  assert_function(eff_measure)
+  assert_function(eff_jacobian)
+  assert_vector(pair)
+  assert(
+    test_subset(pair, names(object)),
+    test_integerish(pair, lower = 1L, upper = length(object))
+  )
+  trt_effect <- unname(eff_measure(object[pair]))
+  if (!is.null(variance)) {
+    inner_variance <- variance(object, ...)[pair, pair]
+    if (missing(eff_jacobian)) {
+      trt_jac <- numDeriv::jacobian(eff_measure, object[pair])
+    } else {
+      trt_jac <- eff_jacobian(object[pair])
+    }
+    trt_var <- trt_jac %*% inner_variance %*% t(trt_jac)
+  } else {
+    trt_var <- diag(NULL)
+  }
+  pair_names <- outer(pair, pair, FUN = paste, sep = " - ")
+  structure(
+    .Data = trt_effect,
+    name = pair_names[lower.tri(pair_names)],
+    variance = diag(trt_var),
+    class = "treatment_effect"
+  )
+}
+
+#' @rdname treatment_effect
+difference <- function(object, pair = names(object), variance = vcovANHECOVA) {
+  treatment_effect(object, pair, variance, eff_measure = h_diff, eff_jacobian = h_jac_diff)
+}
+#' @rdname treatment_effect
+risk_ratio <- function(object, pair = names(object), variance = vcovANHECOVA) {
+  treatment_effect(object, pair, variance, eff_measure = h_ratio, eff_jacobian = h_jac_ratio)
+}
+#' @rdname treatment_effect
+odds_ratio <- function(object, pair = names(object), variance = vcovANHECOVA) {
+  treatment_effect(object, pair, variance, eff_measure = h_odds_ratio, eff_jacobian = h_jac_odds_ratio)
+}
+
+h_diff <- function(x) {
+  assert_numeric(x)
+  d <- outer(x, x, `-`)
+  d[lower.tri(d)]
+}
+h_jac_diff <- function(x) {
+  assert_numeric(x)
+  n <- length(x)
+  if (n == 2) {
+    matrix(c(-1, 1), nrow = 1L)
+  } else {
+    jacobian(h_diff, x)
+  }
+}
+
+h_ratio <- function(x) {
+  assert_numeric(x, lower = 0)
+  d <- outer(x, x, `/`)
+  d[lower.tri(d)]
+}
+h_jac_ratio <- function(x) {
+  assert_numeric(x, lower = 0)
+  n <- length(x)
+  if (n == 2) {
+    matrix(c(- x[2] / x[1]^2, 1 / x[1]), nrow = 1L)
+  } else {
+    jacobian(h_ratio, x)
+  }
+}
+
+h_odds_ratio <- function(x) {
+  assert_numeric(x, lower = 0, upper = 1)
+  y <- x / (1 - x)
+  h_ratio(y)
+}
+
+h_jac_odds_ratio <- function(x) {
+  assert_numeric(x, lower = 0, upper = 1)
+  n <- length(x)
+  if (n == 2) {
+    matrix(c(-x[2] / ((1 - x[2]) * x[1]^2), (1 - x[1]) / ((1 - x[2])^2 * x[1])), nrow = 1L)
+  } else {
+    jacobian(h_odds_ratio, x)
+  }
+}
---
+++ tests/testthat/test-treatment_effect.R
@@ -0,0 +1,137 @@
+test_that("h_diff works as expected", {
+  x <- c(1, 3, 2)
+  expect_identical(
+    h_diff(x),
+    c(2, 1, -1)
+  )
+  x <- c(1, 2)
+  expect_identical(
+    h_diff(x),
+    c(1)
+  )
+})
+
+test_that("h_jac_diff work as expected", {
+  x <- c(1, 3, 2)
+  expect_identical(
+    h_jac_diff(x),
+    matrix(c(-1, -1, 0, 1, 0, -1, 0, 1, 1), nrow = 3),
+    tolerance = 1e-8
+  )
+  x <- c(1, 2)
+  expect_identical(
+    h_jac_diff(x),
+    matrix(c(-1, 1), nrow = 1),
+    tolerance = 1e-8
+  )
+})
+
+test_that("h_ratio works as expected", {
+  x <- c(1, 3, 2)
+  expect_identical(
+    h_ratio(x),
+    c(3, 2, 2 / 3)
+  )
+  x <- c(1, 2)
+  expect_identical(
+    h_ratio(x),
+    c(2)
+  )
+})
+
+test_that("h_jac_ratio work as expected", {
+  x <- c(0.2, 0.5, 0.6)
+  expect_identical(
+    h_jac_ratio(x),
+    matrix(c(-12.5, -15, 0, 5, 0, -2.4, 0, 5, 2), nrow = 3),
+    tolerance = 1e-8
+  )
+  x <- c(0.2, 0.5)
+  expect_identical(
+    h_jac_ratio(x),
+    matrix(c(-12.5, 5), nrow = 1),
+    tolerance = 1e-8
+  )
+})
+
+test_that("h_odds_ratio works as expected", {
+  x <- c(0.5, 0.4, 0.6)
+  expect_identical(
+    h_odds_ratio(x),
+    c(2/3, 1.5, 2.25),
+    tolerance = 1e-8
+  )
+  x <- c(0.5, 0.4)
+  expect_identical(
+    h_odds_ratio(x),
+    2 / 3,
+    tolerance = 1e-8
+  )
+})
+
+test_that("h_jac_odds_ratio work as expected", {
+  x <- c(0.2, 0.5, 0.6)
+  expect_identical(
+    h_jac_odds_ratio(x),
+    matrix(c(-25, -37.5, 0, 16, 0, -6, 0, 25, 6.25), nrow = 3),
+    tolerance = 1e-8
+  )
+  x <- c(0.2, 0.5)
+  expect_identical(
+    h_jac_odds_ratio(x),
+    matrix(c(-25, 16), nrow = 1),
+    tolerance = 1e-8
+  )
+})
+
+test_that("treatment_effect works as expected", {
+  pc <- predict_counterfactual(fit_binom, treatment ~ s1)
+  df <- expect_silent(difference(pc))
+  rr <- expect_silent(risk_ratio(pc))
+  or <- expect_silent(odds_ratio(pc))
+  expect_identical(
+    df,
+    c(0.22459921, 0.26528993, 0.04069073),
+    ignore_attr = TRUE,
+    tolerance = 1e-6
+  )
+  expect_identical(
+    attr(df, "name"),
+    c("trt1 - pbo", "trt2 - pbo", "trt2 - trt1")
+  )
+  expect_identical(
+    attr(df, "variance"),
+    c(0.002276376, 0.002259492, 0.002298305),
+    tolerance = 1e-6
+  )
+  expect_identical(
+    rr,
+    c(1.630726, 1.744994, 1.070072),
+    ignore_attr = TRUE,
+    tolerance = 1e-6
+  )
+  expect_identical(
+    attr(rr, "name"),
+    c("trt1 - pbo", "trt2 - pbo", "trt2 - trt1")
+  )
+  expect_identical(
+    attr(rr, "variance"),
+    c(0.032539074, 0.035867956, 0.007316219),
+    tolerance = 1e-6
+  )
+  expect_identical(
+    or,
+    c(2.504219, 2.967691, 1.185076),
+    ignore_attr = TRUE,
+    tolerance = 1e-6
+  )
+  expect_identical(
+    attr(or, "name"),
+    c("trt1 - pbo", "trt2 - pbo", "trt2 - trt1")
+  )
+  expect_identical(
+    attr(or, "variance"),
+    c(0.25578808, 0.36889840, 0.05635703),
+    tolerance = 1e-6
+  )
+})
