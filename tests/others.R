a9415614ac3a2a355dd67369f2898e0e1e23f8f4
Daniel Sabanes Bove <daniel.sabanesbove@gmx.net>
2025-09-01 16:46:49 +0800

    fix special test too

--- tests/others.R
+++ tests/others.R
@@ -6,7 +6,7 @@ surv_obj <- with(surv_data, survival::Surv(time, status))
 # This works with coxph e.g.:
 example <- survival::coxph(surv_obj ~ sex, data = surv_data)
 result <- RobinCar2:::h_prep_survival_input(
-  formula = surv_obj ~ sex,
+  formula = surv_obj ~ 1,
   # We have another restriction here to avoid ambiguity, therefore
   # need to remove the time and status columns from the data.
   data = subset(surv_data, select = -c(time, status)),
